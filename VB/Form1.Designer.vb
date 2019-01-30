601251fe81a2d550ca29f119c7bbbf89170eb9c8
DevExpressExampleBot <fakeEmail>
2019-01-30 18:27:58 +0300

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,105 +1,101 @@
-Imports Microsoft.VisualBasic
-Imports System
-Namespace Q183147
-	Partial Public Class Form1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+﻿Namespace Q183147
+    Partial Public Class Form1
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#Region "Windows Form Designer generated code"
+        #Region "Windows Form Designer generated code"
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
-			Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
-			Me.gridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.gridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
-			Me.gridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			' 
-			' gridControl1
-			' 
-			Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.gridControl1.Location = New System.Drawing.Point(0, 0)
-			Me.gridControl1.MainView = Me.gridView1
-			Me.gridControl1.Name = "gridControl1"
-			Me.gridControl1.Size = New System.Drawing.Size(824, 479)
-			Me.gridControl1.TabIndex = 0
-			Me.gridControl1.UseEmbeddedNavigator = True
-			Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
-			' 
-			' gridView1
-			' 
-			Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.gridColumn1, Me.gridColumn2, Me.gridColumn3})
-			Me.gridView1.GridControl = Me.gridControl1
-			Me.gridView1.GroupCount = 1
-			Me.gridView1.Name = "gridView1"
-			Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() { New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.gridColumn3, DevExpress.Data.ColumnSortOrder.Ascending)})
-'			Me.gridView1.FocusedRowChanged += New DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(Me.gridView1_FocusedRowChanged);
-			' 
-			' gridColumn1
-			' 
-			Me.gridColumn1.Caption = "gridColumn1"
-			Me.gridColumn1.FieldName = "ID"
-			Me.gridColumn1.Name = "gridColumn1"
-			Me.gridColumn1.Visible = True
-			Me.gridColumn1.VisibleIndex = 0
-			' 
-			' gridColumn2
-			' 
-			Me.gridColumn2.Caption = "gridColumn2"
-			Me.gridColumn2.FieldName = "Name"
-			Me.gridColumn2.Name = "gridColumn2"
-			Me.gridColumn2.Visible = True
-			Me.gridColumn2.VisibleIndex = 1
-			' 
-			' gridColumn3
-			' 
-			Me.gridColumn3.Caption = "gridColumn3"
-			Me.gridColumn3.FieldName = "Category"
-			Me.gridColumn3.Name = "gridColumn3"
-			Me.gridColumn3.Visible = True
-			Me.gridColumn3.VisibleIndex = 2
-			' 
-			' Form1
-			' 
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(824, 479)
-			Me.Controls.Add(Me.gridControl1)
-			Me.Name = "Form1"
-			Me.Text = "Form1"
-'			Me.Load += New System.EventHandler(Me.Form1_Load);
-			CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-			CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.gridControl1 = New DevExpress.XtraGrid.GridControl()
+            Me.gridView1 = New DevExpress.XtraGrid.Views.Grid.GridView()
+            Me.gridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.gridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
+            Me.gridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            ' 
+            ' gridControl1
+            ' 
+            Me.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.gridControl1.Location = New System.Drawing.Point(0, 0)
+            Me.gridControl1.MainView = Me.gridView1
+            Me.gridControl1.Name = "gridControl1"
+            Me.gridControl1.Size = New System.Drawing.Size(824, 479)
+            Me.gridControl1.TabIndex = 0
+            Me.gridControl1.UseEmbeddedNavigator = True
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+            ' 
+            ' gridView1
+            ' 
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.gridColumn1, Me.gridColumn2, Me.gridColumn3})
+            Me.gridView1.GridControl = Me.gridControl1
+            Me.gridView1.GroupCount = 1
+            Me.gridView1.Name = "gridView1"
+            Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() { New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.gridColumn3, DevExpress.Data.ColumnSortOrder.Ascending)})
+            ' 
+            ' gridColumn1
+            ' 
+            Me.gridColumn1.Caption = "gridColumn1"
+            Me.gridColumn1.FieldName = "ID"
+            Me.gridColumn1.Name = "gridColumn1"
+            Me.gridColumn1.Visible = True
+            Me.gridColumn1.VisibleIndex = 0
+            ' 
+            ' gridColumn2
+            ' 
+            Me.gridColumn2.Caption = "gridColumn2"
+            Me.gridColumn2.FieldName = "Name"
+            Me.gridColumn2.Name = "gridColumn2"
+            Me.gridColumn2.Visible = True
+            Me.gridColumn2.VisibleIndex = 1
+            ' 
+            ' gridColumn3
+            ' 
+            Me.gridColumn3.Caption = "gridColumn3"
+            Me.gridColumn3.FieldName = "Category"
+            Me.gridColumn3.Name = "gridColumn3"
+            Me.gridColumn3.Visible = True
+            Me.gridColumn3.VisibleIndex = 2
+            ' 
+            ' Form1
+            ' 
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(824, 479)
+            Me.Controls.Add(Me.gridControl1)
+            Me.Name = "Form1"
+            Me.Text = "Form1"
+            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
+            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
 
-		End Sub
+        End Sub
 
-		#End Region
+        #End Region
 
-		Private gridControl1 As DevExpress.XtraGrid.GridControl
-		Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
-		Private gridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
-		Private gridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
-		Private gridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
-	End Class
+        Private gridControl1 As DevExpress.XtraGrid.GridControl
+        Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+        Private gridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+        Private gridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+        Private gridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
+    End Class
 End Namespace
 
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,83 +1,63 @@
-Imports Microsoft.VisualBasic
-Imports DevExpress.XtraGrid
+﻿Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Views.Grid
 Imports System
 Imports System.ComponentModel
 Imports System.Windows.Forms
 
 Namespace Q183147
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits Form
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			Dim rand As New Random()
-			Dim list = New BindingList(Of Item)()
-			For i As Integer = 0 To 49
-				list.Add(New Item() With {.ID = i, .Name = "Name" & i, .Category = rand.Next(0, 5)})
-			Next i
-			gridControl1.DataSource = list
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private Sub gridView1_FocusedRowChanged(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs) Handles gridView1.FocusedRowChanged
-			Dim focusedRowHandle As Integer = -1
-			If e.FocusedRowHandle = GridControl.NewItemRowHandle OrElse e.FocusedRowHandle = GridControl.AutoFilterRowHandle Then
-				Return
-			End If
-			Dim view As GridView = CType(sender, GridView)
-			If e.FocusedRowHandle < 0 Then
-				If e.PrevFocusedRowHandle = GridControl.InvalidRowHandle Then
-					focusedRowHandle = 0
-				ElseIf Control.MouseButtons = MouseButtons.Left OrElse Control.MouseButtons = MouseButtons.Right Then
-					focusedRowHandle = e.PrevFocusedRowHandle
-				Else
-					Dim prevRow As Integer = view.GetVisibleIndex(e.PrevFocusedRowHandle)
-					Dim currRow As Integer = view.GetVisibleIndex(e.FocusedRowHandle)
-					If prevRow > currRow Then
-						focusedRowHandle = e.PrevFocusedRowHandle - 1
-					Else
-						focusedRowHandle = e.PrevFocusedRowHandle + 1
-					End If
-					If focusedRowHandle < 0 Then
-						focusedRowHandle = 0
-					End If
-					If focusedRowHandle >= view.DataRowCount Then
-						focusedRowHandle = view.DataRowCount - 1
-					End If
-				End If
-				view.FocusedRowHandle = If(focusedRowHandle < 0, 0, focusedRowHandle)
-			End If
-		End Sub
-		Public Class Item
-			Private privateID As Integer
-			Public Property ID() As Integer
-				Get
-					Return privateID
-				End Get
-				Set(ByVal value As Integer)
-					privateID = value
-				End Set
-			End Property
-			Private privateName As String
-			Public Property Name() As String
-				Get
-					Return privateName
-				End Get
-				Set(ByVal value As String)
-					privateName = value
-				End Set
-			End Property
-			Private privateCategory As Integer
-			Public Property Category() As Integer
-				Get
-					Return privateCategory
-				End Get
-				Set(ByVal value As Integer)
-					privateCategory = value
-				End Set
-			End Property
-		End Class
-	End Class
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            Dim rand As New Random()
+            Dim list = New BindingList(Of Item)()
+            For i As Integer = 0 To 49
+                list.Add(New Item() With { _
+                    .ID = i, _
+                    .Name = "Name" & i, _
+                    .Category = rand.Next(0, 5) _
+                })
+            Next i
+            gridControl1.DataSource = list
+        End Sub
+
+        Private Sub gridView1_FocusedRowChanged(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs) Handles gridView1.FocusedRowChanged
+            Dim focusedRowHandle As Integer = -1
+            If e.FocusedRowHandle = GridControl.NewItemRowHandle OrElse e.FocusedRowHandle = GridControl.AutoFilterRowHandle Then
+                Return
+            End If
+            Dim view As GridView = DirectCast(sender, GridView)
+            If e.FocusedRowHandle < 0 Then
+                If e.PrevFocusedRowHandle = GridControl.InvalidRowHandle Then
+                    focusedRowHandle = 0
+                ElseIf Control.MouseButtons = MouseButtons.Left OrElse Control.MouseButtons = MouseButtons.Right Then
+                    focusedRowHandle = e.PrevFocusedRowHandle
+                Else
+                    Dim prevRow As Integer = view.GetVisibleIndex(e.PrevFocusedRowHandle)
+                    Dim currRow As Integer = view.GetVisibleIndex(e.FocusedRowHandle)
+                    If prevRow > currRow Then
+                        focusedRowHandle = e.PrevFocusedRowHandle - 1
+                    Else
+                        focusedRowHandle = e.PrevFocusedRowHandle + 1
+                    End If
+                    If focusedRowHandle < 0 Then
+                        focusedRowHandle = 0
+                    End If
+                    If focusedRowHandle >= view.DataRowCount Then
+                        focusedRowHandle = view.DataRowCount - 1
+                    End If
+                End If
+                view.FocusedRowHandle = If(focusedRowHandle < 0, 0, focusedRowHandle)
+            End If
+        End Sub
+        Public Class Item
+            Public Property ID() As Integer
+            Public Property Name() As String
+            Public Property Category() As Integer
+        End Class
+    End Class
 End Namespace
