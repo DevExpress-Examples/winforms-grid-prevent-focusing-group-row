08782e7be5ca107b62289f3c6111617d31ec6a67
DevExpressExampleBot <fakeEmail>
2023-09-14 22:30:13 +0400

    Source auto update [skip ci]

--- VB/Form1.Designer.vb
+++ VB/Form1.Designer.vb
@@ -1,5 +1,7 @@
-﻿Namespace Q183147
-    Partial Public Class Form1
+Namespace Q183147
+
+    Partial Class Form1
+
         ''' <summary>
         ''' Required designer variable.
         ''' </summary>
@@ -10,14 +12,14 @@
         ''' </summary>
         ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-            If disposing AndAlso (components IsNot Nothing) Then
-                components.Dispose()
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
             End If
+
             MyBase.Dispose(disposing)
         End Sub
 
-        #Region "Windows Form Designer generated code"
-
+#Region "Windows Form Designer generated code"
         ''' <summary>
         ''' Required method for Designer support - do not modify
         ''' the contents of this method with the code editor.
@@ -28,8 +30,8 @@
             Me.gridColumn1 = New DevExpress.XtraGrid.Columns.GridColumn()
             Me.gridColumn2 = New DevExpress.XtraGrid.Columns.GridColumn()
             Me.gridColumn3 = New DevExpress.XtraGrid.Columns.GridColumn()
-            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).BeginInit()
-            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).BeginInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).BeginInit()
             Me.SuspendLayout()
             ' 
             ' gridControl1
@@ -41,15 +43,16 @@
             Me.gridControl1.Size = New System.Drawing.Size(824, 479)
             Me.gridControl1.TabIndex = 0
             Me.gridControl1.UseEmbeddedNavigator = True
-            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() { Me.gridView1})
+            Me.gridControl1.ViewCollection.AddRange(New DevExpress.XtraGrid.Views.Base.BaseView() {Me.gridView1})
             ' 
             ' gridView1
             ' 
-            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() { Me.gridColumn1, Me.gridColumn2, Me.gridColumn3})
+            Me.gridView1.Columns.AddRange(New DevExpress.XtraGrid.Columns.GridColumn() {Me.gridColumn1, Me.gridColumn2, Me.gridColumn3})
             Me.gridView1.GridControl = Me.gridControl1
             Me.gridView1.GroupCount = 1
             Me.gridView1.Name = "gridView1"
-            Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() { New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.gridColumn3, DevExpress.Data.ColumnSortOrder.Ascending)})
+            Me.gridView1.SortInfo.AddRange(New DevExpress.XtraGrid.Columns.GridColumnSortInfo() {New DevExpress.XtraGrid.Columns.GridColumnSortInfo(Me.gridColumn3, DevExpress.Data.ColumnSortOrder.Ascending)})
+            AddHandler Me.gridView1.FocusedRowChanged, New DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventHandler(AddressOf Me.gridView1_FocusedRowChanged)
             ' 
             ' gridColumn1
             ' 
@@ -83,19 +86,21 @@
             Me.Controls.Add(Me.gridControl1)
             Me.Name = "Form1"
             Me.Text = "Form1"
-            CType(Me.gridControl1, System.ComponentModel.ISupportInitialize).EndInit()
-            CType(Me.gridView1, System.ComponentModel.ISupportInitialize).EndInit()
+            AddHandler Me.Load, New System.EventHandler(AddressOf Me.Form1_Load)
+            CType((Me.gridControl1), System.ComponentModel.ISupportInitialize).EndInit()
+            CType((Me.gridView1), System.ComponentModel.ISupportInitialize).EndInit()
             Me.ResumeLayout(False)
-
         End Sub
 
-        #End Region
-
+#End Region
         Private gridControl1 As DevExpress.XtraGrid.GridControl
-        Private WithEvents gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
+        Private gridView1 As DevExpress.XtraGrid.Views.Grid.GridView
+
         Private gridColumn1 As DevExpress.XtraGrid.Columns.GridColumn
+
         Private gridColumn2 As DevExpress.XtraGrid.Columns.GridColumn
+
         Private gridColumn3 As DevExpress.XtraGrid.Columns.GridColumn
     End Class
 End Namespace
-
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,40 +1,36 @@
-﻿Imports DevExpress.XtraGrid
+Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Views.Grid
 Imports System
 Imports System.ComponentModel
 Imports System.Windows.Forms
 
 Namespace Q183147
-    Partial Public Class Form1
+
+    Public Partial Class Form1
         Inherits Form
 
         Public Sub New()
             InitializeComponent()
         End Sub
 
-        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
-            Dim rand As New Random()
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs)
+            Dim rand As Random = New Random()
             Dim list = New BindingList(Of Item)()
-            For i As Integer = 0 To 49
-                list.Add(New Item() With { _
-                    .ID = i, _
-                    .Name = "Name" & i, _
-                    .Category = rand.Next(0, 5) _
-                })
-            Next i
+            For i As Integer = 0 To 50 - 1
+                list.Add(New Item() With {.ID = i, .Name = "Name" & i, .Category = rand.Next(0, 5)})
+            Next
+
             gridControl1.DataSource = list
         End Sub
 
-        Private Sub gridView1_FocusedRowChanged(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Base.FocusedRowChangedEventArgs) Handles gridView1.FocusedRowChanged
+        Private Sub gridView1_FocusedRowChanged(ByVal sender As Object, ByVal e As Views.Base.FocusedRowChangedEventArgs)
             Dim focusedRowHandle As Integer = -1
-            If e.FocusedRowHandle = GridControl.NewItemRowHandle OrElse e.FocusedRowHandle = GridControl.AutoFilterRowHandle Then
-                Return
-            End If
-            Dim view As GridView = DirectCast(sender, GridView)
+            If e.FocusedRowHandle = GridControl.NewItemRowHandle OrElse e.FocusedRowHandle = GridControl.AutoFilterRowHandle Then Return
+            Dim view As GridView = CType(sender, GridView)
             If e.FocusedRowHandle < 0 Then
                 If e.PrevFocusedRowHandle = GridControl.InvalidRowHandle Then
                     focusedRowHandle = 0
-                ElseIf Control.MouseButtons = MouseButtons.Left OrElse Control.MouseButtons = MouseButtons.Right Then
+                ElseIf MouseButtons = MouseButtons.Left OrElse MouseButtons = MouseButtons.Right Then
                     focusedRowHandle = e.PrevFocusedRowHandle
                 Else
                     Dim prevRow As Integer = view.GetVisibleIndex(e.PrevFocusedRowHandle)
@@ -44,20 +40,22 @@ Namespace Q183147
                     Else
                         focusedRowHandle = e.PrevFocusedRowHandle + 1
                     End If
-                    If focusedRowHandle < 0 Then
-                        focusedRowHandle = 0
-                    End If
-                    If focusedRowHandle >= view.DataRowCount Then
-                        focusedRowHandle = view.DataRowCount - 1
-                    End If
+
+                    If focusedRowHandle < 0 Then focusedRowHandle = 0
+                    If focusedRowHandle >= view.DataRowCount Then focusedRowHandle = view.DataRowCount - 1
                 End If
+
                 view.FocusedRowHandle = If(focusedRowHandle < 0, 0, focusedRowHandle)
             End If
         End Sub
+
         Public Class Item
-            Public Property ID() As Integer
-            Public Property Name() As String
-            Public Property Category() As Integer
+
+            Public Property ID As Integer
+
+            Public Property Name As String
+
+            Public Property Category As Integer
         End Class
     End Class
 End Namespace
